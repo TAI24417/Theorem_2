1a54baeb599af54edcfc54a28568e063ac0c895d
WangYiran01 <129300550+WangYiran01@users.noreply.github.com>
2024-07-02 10:28:47 +0800

    Update idt_41.lean

--- Theorem/IDT_1to100/IDT_41to50/idt_41.lean
+++ Theorem/IDT_1to100/IDT_41to50/idt_41.lean
@@ -5,6 +5,7 @@ import Theorem.IDT_1to100.IDT_1to10.idt_6
 import Theorem.Lemma.two_eq_one_add_one
 import Theorem.Lemma.three_eq_two_add_one
 
+
 open Nat
 open Finset
 open BigOperators
@@ -12,12 +13,66 @@ open BigOperators
 
 -- theorem lemma_1 {k : ℕ}: (-1:ℝ) ^ k = (-1:ℝ) ^ (k + 2 -2) := by
 --     congr
+theorem tree_eq_two_add_one: 3= 2+1 :=by
+  linarith
 
 theorem lemma_2 {n : ℕ} : ∑ k in Ico 0 (n+1) , (-1:ℝ) ^ (k +2) * choose (n+2) (k+2) = n+1 :=by
-    let f: ℕ → ℝ := fun k =>  (-1:ℝ) ^ k * choose (n+2) k
-    rw [sum_Ico_add' f]
+  let f: ℕ → ℝ := fun k =>  (-1:ℝ) ^ k * choose (n+2) k
+  rw [sum_Ico_add' f]
+  ring_nf
+  rw [add_comm 3 n]
+  have h₁: ∑ x in Ico 0 (n + 3), f x =  f 0 + f 1 + ∑ x in Ico 2 (n + 3), f x :=by
+    rw [sum_eq_sum_Ico_succ_bot,sum_eq_sum_Ico_succ_bot]
+    simp
+    ring
+    linarith
+
+    have hh : 0 + 3 ≤ n + 3 := Nat.add_le_add_right (Nat.zero_le n) 3
+    rw [Nat.zero_add] at hh
+    exact lt_of_lt_of_le (Nat.zero_lt_succ 2) hh
+  have h_2: f 0 = 1 :=by
     ring_nf
-    rw [add_comm 3 n]
+    rw [choose_zero_right]
+    ring
+
+  have h_3 : f 1 = - (n+2) :=by
+    ring_nf
+    rw [choose_one_right]
+    simp
+    ring
+
+  have h_4: ∑ x in Ico 0 (n + 3), f x  = 0 := by
+    ring_nf
+    rw [←range_eq_Ico]
+    rw [add_comm 2 n,add_comm 3 n]
+    rw [tree_eq_two_add_one,←add_assoc]
+
+    rw [sum_congr rfl fun a ha => by
+      rw [mul_comm]
+    ]
+    have h0: ∑ a in range (n + 2 + 1), (-1:ℝ) ^ a * (Nat.choose (n + 2) a) = ∑ a in range (n + 2 + 1), (-1:ℤ) ^ a * (Nat.choose (n + 2) a) :=by
+      simp
+    rw [h0]
+    have hn2: (n+2) ≠ 0 :=by
+      simp
+    rw [Int.alternating_sum_range_choose_of_ne hn2]
+    simp
+
+  rw [h_4,h_2,h_3] at h₁
+  symm at h₁
+  rw [add_comm 1] at h₁
+  ring_nf at h₁
+  rw [add_comm] at h₁
+
+  rw [sub_eq_neg_add,←neg_add,add_neg_eq_iff_eq_add,zero_add,add_comm 3] at h₁
+  symm at h₁
+  rw [h₁, sum_congr rfl fun a ha => by
+    rw [mul_comm]
+  ]
+theorem neg_one_pow_add {k:ℕ}: (-1:ℝ) ^ k = (-1:ℝ) ^ (k+2):= by
+  rw [pow_add]
+  rw [neg_one_pow_two]
+  ring
 
 theorem idt_41 { n : ℕ }: ∑ k in Ico 0 (n+1),  (-1:ℝ)^k *  (1/ (k+2)) * (1/ (k+1)) * n.choose (k) = 1 / (n +2 ) :=by
  sorry
