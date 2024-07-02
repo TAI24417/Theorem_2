f6d63059597163f8f4ee10e65ee66dd11d9a7f9e
WangYiran01 <129300550+WangYiran01@users.noreply.github.com>
2024-07-02 11:52:15 +0800

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -4,6 +4,7 @@ open Lake DSL
 package «Theorem» where
   -- add package configuration options here
 
+ @[default_target]
 lean_lib «Theorem» where
   -- add library configuration options here
   -- srcDir := "Theorem_Library_MZ"
@@ -11,10 +12,10 @@ lean_lib «Theorem» where
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"@"1ff3bf341c6a14611bd2f7f689853d12f06c1694"
 
--- @[default_target]
--- lean_exe «theorem» where
---   root := `Main
+@[default_target]
+lean_exe «theorem» where
+  root := `Main
 --   -- Enables the use of the Lean interpreter by the executable (e.g.,
 --   -- `runFrontend`) at the expense of increased binary size on Linux.
 --   -- Remove this line if you do not need such functionality.
---   supportInterpreter := true
+  supportInterpreter := true
